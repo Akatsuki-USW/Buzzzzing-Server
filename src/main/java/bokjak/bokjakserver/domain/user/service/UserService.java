5c2a5bb05b99e7f7b6817bbb4dc47c5a6e88f23b
wonseok2877 <wonseok2877@gmail.com>
2023-07-14 22:37:23 +0900

    test: 로케이션 테스트 코드

--- src/main/java/bokjak/bokjakserver/domain/user/service/UserService.java
+++ src/main/java/bokjak/bokjakserver/domain/user/service/UserService.java
@@ -11,13 +11,10 @@ import bokjak.bokjakserver.domain.user.repository.UserRepository;
 import bokjak.bokjakserver.util.CustomEncryptUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
 import java.util.regex.Pattern;
 
 import static bokjak.bokjakserver.config.security.SecurityUtils.getCurrentUserSocialEmail;
