e3da54bae367d046f027b567fbf08458031e910e
WooYoungDoo <duden5000@naver.com>
2023-07-31 11:27:42 +0900

    feat: 메일 발송 test

--- src/main/java/bokjak/bokjakserver/domain/test/TestController.java
+++ src/main/java/bokjak/bokjakserver/domain/test/TestController.java
@@ -6,8 +6,10 @@ import bokjak.bokjakserver.domain.notification.dto.NotificationDto.NotifyParams;
 import bokjak.bokjakserver.domain.notification.model.NotificationType;
 import bokjak.bokjakserver.domain.notification.service.NotificationService;
 import bokjak.bokjakserver.domain.user.model.User;
+import bokjak.bokjakserver.domain.user.service.SleepingUserService;
 import bokjak.bokjakserver.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +22,7 @@ public class TestController {
     private final UserService userService;
     private final NotificationService notificationService;
 
+    private final SleepingUserService sleepingUserService;
 
     @PostMapping("/push/self")
     public ApiResponse<NotificationDto.NotificationListResponse> testCreateNotificationMySelf() {
@@ -33,5 +36,16 @@ public class TestController {
                 .build();
         notificationService.pushMessage(params);
         return ApiResponse.success(notificationService.getMyNotifications(currentUser));
+    }
+
+    @GetMapping("/send/email")
+    public String testSendEmailMySelf() {
+        long beforeTime = System.currentTimeMillis();
+        User currentUser = userService.getCurrentUser();
+        sleepingUserService.sendMail(currentUser.getEmail());
+        long afterTime = System.currentTimeMillis();
+        long diffTime = afterTime-beforeTime;
+        System.out.println("실행 시간: " + diffTime);
+        return "good";
     }
 }
