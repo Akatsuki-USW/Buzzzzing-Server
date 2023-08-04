06019686b5967452a31df731922b6721ae565d5e
WooYoungDoo <duden5000@naver.com>
2023-08-04 15:28:55 +0900

    refactor: ban url 변경

--- src/main/java/bokjak/bokjakserver/domain/ban/controller/BanController.java
+++ src/main/java/bokjak/bokjakserver/domain/ban/controller/BanController.java
@@ -1,27 +1,33 @@
 package bokjak.bokjakserver.domain.ban.controller;
 
+import bokjak.bokjakserver.common.constant.SwaggerConstants;
 import bokjak.bokjakserver.common.dto.ApiResponse;
 import bokjak.bokjakserver.config.security.PrincipalDetails;
-import bokjak.bokjakserver.domain.ban.dto.BanDto;
 import bokjak.bokjakserver.domain.ban.dto.BanDto.BanListResponse;
 import bokjak.bokjakserver.domain.ban.service.BanService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static bokjak.bokjakserver.common.constant.SwaggerConstants.*;
 import static bokjak.bokjakserver.common.dto.ApiResponse.success;
 
 @RestController
-@RequestMapping("/mypage/ban")
+@RequestMapping("/bans")
 @RequiredArgsConstructor
+@Tag(name = TAG_BAN, description = TAG_BAN_DESCRIPTION)
 public class BanController {
 
     private final BanService banService;
 
-    @GetMapping("/my")
+    @GetMapping("/me")
+    @Operation(summary = BAN_ME)
+    @SecurityRequirement(name = SwaggerConstants.SECURITY_SCHEME_NAME)
     public ApiResponse<BanListResponse> getMyBan(@AuthenticationPrincipal PrincipalDetails principalDetails) {
         return success(banService.getMyBanList(principalDetails.getUserId()));
     }
