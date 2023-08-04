f1ab4c7b5e534f82cc373b040ae36a1e9cdaaf25
wonseok2877 <wonseok2877@gmail.com>
2023-08-04 14:18:48 +0900

    fix: 대댓글 오래된 순으로 수정 + totalElements 오류 해결

--- src/main/java/bokjak/bokjakserver/domain/comment/repository/CommentRepositoryCustom.java
+++ src/main/java/bokjak/bokjakserver/domain/comment/repository/CommentRepositoryCustom.java
@@ -1,12 +1,15 @@
 package bokjak.bokjakserver.domain.comment.repository;
 
 import bokjak.bokjakserver.domain.comment.model.Comment;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 
 public interface CommentRepositoryCustom {
-    Page<Comment> findAllParentBySpotExceptBlockedAuthors(Pageable pageable, Long cursorId, Long spotId, Long userId);
+    Slice<Comment> findAllParentBySpotExceptBlockedAuthors(Pageable pageable, Long cursorId, Long spotId, Long userId);
 
-    Page<Comment> findAllChildrenByParentExceptBlockedAuthors(Pageable pageable, Long cursorId, Long parentId, Long userId);
+    Long countAllParentBySpotExceptBlockedAuthors(Long spotId, Long userId);
 
+    Slice<Comment> findAllChildrenByParentExceptBlockedAuthors(Pageable pageable, Long cursorId, Long parentId, Long userId);
+
+    Long countAllChildrenByParentExceptBlockedAuthors(Long parentId, Long userId);
 }
