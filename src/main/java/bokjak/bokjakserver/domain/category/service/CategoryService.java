9fc185c9d3b6bfcc70668f538854849eb9f5a70d
wonseok2877 <wonseok2877@gmail.com>
2023-10-02 19:04:37 +0900

    refactor: getAllCategory Last-Modified HTTP 캐싱

--- src/main/java/bokjak/bokjakserver/domain/category/service/CategoryService.java
+++ src/main/java/bokjak/bokjakserver/domain/category/service/CategoryService.java
@@ -2,6 +2,7 @@ package bokjak.bokjakserver.domain.category.service;
 
 import bokjak.bokjakserver.common.dummy.DummyLocationCategory;
 import bokjak.bokjakserver.common.dummy.DummySpotCategory;
+import bokjak.bokjakserver.common.model.BaseEntity;
 import bokjak.bokjakserver.domain.category.dto.CategoryDto.*;
 import bokjak.bokjakserver.domain.category.model.LocationCategory;
 import bokjak.bokjakserver.domain.category.model.SpotCategory;
@@ -11,7 +12,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 @Transactional(readOnly = true)
@@ -20,14 +23,27 @@ public class CategoryService {
     private final LocationCategoryRepository locationCategoryRepository;
     private final SpotCategoryRepository spotCategoryRepository;
 
-    public AllCategoryResponse getAllCategory(){
-        List<LocationCategoryResponse> locationCategoryResponses = locationCategoryRepository.findAll()
+    public AllCategories getAllCategory() {
+        List<LocationCategory> locationCategoryList = locationCategoryRepository.findAll();
+        List<SpotCategory> spotCategoryList = spotCategoryRepository.findAll();
+
+        LocalDateTime lastUpdatedTime = resolveLastUpdatedTime(locationCategoryList, spotCategoryList);
+
+        List<LocationCategoryResponse> locationCategoryResponses = locationCategoryList
                 .stream().map(LocationCategoryResponse::of)
                 .toList();
-        List<SpotCategoryResponse> spotCategoryResponses = spotCategoryRepository.findAll()
+        List<SpotCategoryResponse> spotCategoryResponses = spotCategoryList
                 .stream().map(SpotCategoryResponse::of)
                 .toList();
-        return AllCategoryResponse.of(locationCategoryResponses, spotCategoryResponses);
+        return AllCategories.of(locationCategoryResponses, spotCategoryResponses, lastUpdatedTime);
+    }
+
+    private LocalDateTime resolveLastUpdatedTime(List<LocationCategory> locationCategoryList, List<SpotCategory> spotCategoryList) {
+        List<LocalDateTime> updatedTimeList = new java.util.ArrayList<>();
+        updatedTimeList.addAll(locationCategoryList.stream().map(BaseEntity::getUpdatedAt).toList());
+        updatedTimeList.addAll(spotCategoryList.stream().map(BaseEntity::getUpdatedAt).toList());
+
+        return updatedTimeList.stream().max(LocalDateTime::compareTo).orElseThrow(NoSuchElementException::new);
     }
 
     // Dummy 생성 및 테스트용 메서드
