797ff84944627ba0548ee19d2a9d59efde2f9b27
wonseok2877 <wonseok2877@gmail.com>
2023-11-04 10:49:02 +0900

    Revert "refactor: getSimpleLocations Last-Modified HTTP 캐싱"
    This reverts commit 8731c6442c01e676ab27f916a718527952367ddb.

--- src/main/java/bokjak/bokjakserver/domain/location/service/LocationService.java
+++ src/main/java/bokjak/bokjakserver/domain/location/service/LocationService.java
@@ -2,9 +2,7 @@ package bokjak.bokjakserver.domain.location.service;
 
 import bokjak.bokjakserver.common.constant.GlobalConstants;
 import bokjak.bokjakserver.common.dto.PageResponse;
-import bokjak.bokjakserver.common.dto.PageWithLastModified;
 import bokjak.bokjakserver.common.exception.StatusCode;
-import bokjak.bokjakserver.common.model.BaseEntity;
 import bokjak.bokjakserver.domain.bookmark.model.LocationBookmark;
 import bokjak.bokjakserver.domain.bookmark.repository.LocationBookmarkRepository;
 import bokjak.bokjakserver.domain.congestion.dto.CongestionDto.CongestionPrediction;
@@ -34,7 +32,10 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.*;
 import java.time.format.DateTimeFormatter;
-import java.util.*;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 import static bokjak.bokjakserver.domain.congestion.model.CongestionLevel.RELAX;
 
@@ -74,11 +75,12 @@ public class LocationService {
     }
 
     // 로케이션 단순 리스트 조회
-    public PageWithLastModified<LocationSimpleCardResponse> getSimpleLocations(Pageable pageable, Long cursorId) {
+    public PageResponse<LocationSimpleCardResponse> getLocations(Pageable pageable, Long cursorId) {
         Page<Location> locations = locationRepository.getLocations(pageable, cursorId);
+
         Page<LocationSimpleCardResponse> responsePage = locations.map(LocationSimpleCardResponse::of);
 
-        return PageWithLastModified.of(responsePage, resolveLastModifiedAt(locations));
+        return PageResponse.of(responsePage);
     }
 
     // 혼잡도 낮은순 TOP N 조회 : 주간 통계 기반 정렬
@@ -159,12 +161,7 @@ public class LocationService {
         return BookmarkResponse.of(locationId, isBookmarked);
     }
 
-    /* private */
-    private LocalDateTime resolveLastModifiedAt(Page<Location> locationList) {// 가장 마지막에 수정된 시각 추출
-        List<LocalDateTime> lastModifiedTimeList = locationList.stream().map(BaseEntity::getUpdatedAt).toList();
-
-        return lastModifiedTimeList.stream().max(LocalDateTime::compareTo).orElseThrow(NoSuchElementException::new);
-    }
+    /* private 함수 */
 
     private PageResponse<LocationCardResponse> makeLocationCardPageResponse(User currentUser, Page<Location> resultPage) {
         List<Long> bookmarkedLocationIdList = locationBookmarkRepository.findAllByUser(currentUser).stream()
