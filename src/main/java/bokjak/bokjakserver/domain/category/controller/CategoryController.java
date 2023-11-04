121128c7d40d18dcd4fe443282c97b3f7e708387
wonseok2877 <wonseok2877@gmail.com>
2023-11-04 10:49:03 +0900

    Revert "refactor: getAllCategory Last-Modified HTTP 캐싱"
    This reverts commit 9fc185c9d3b6bfcc70668f538854849eb9f5a70d.

--- src/main/java/bokjak/bokjakserver/domain/category/controller/CategoryController.java
+++ src/main/java/bokjak/bokjakserver/domain/category/controller/CategoryController.java
@@ -2,25 +2,14 @@ package bokjak.bokjakserver.domain.category.controller;
 
 import bokjak.bokjakserver.common.constant.SwaggerConstants;
 import bokjak.bokjakserver.common.dto.ApiResponse;
-import bokjak.bokjakserver.domain.category.dto.CategoryDto.AllCategories;
 import bokjak.bokjakserver.domain.category.dto.CategoryDto.AllCategoryResponse;
 import bokjak.bokjakserver.domain.category.service.CategoryService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.CacheControl;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.ServletWebRequest;
-
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
 
 @RestController
 @RequestMapping("/categories")
@@ -31,18 +20,7 @@ public class CategoryController {
 
     @GetMapping
     @Operation(summary = SwaggerConstants.CATEGORY_GET_ALL, description = SwaggerConstants.CATEGORY_GET_ALL_DESCRIPTION)
-    public ResponseEntity<ApiResponse<AllCategoryResponse>> getAllCategory(ServletWebRequest request) {
-        AllCategories allCategories = categoryService.getAllCategory();
-        LocalDateTime lastModifiedAt = allCategories.lastModifiedAt();
-
-        // Last-Modified 검증. 같다면 304 응답
-        if (request.checkNotModified(String.valueOf(lastModifiedAt))) {
-            return null;
-        }
-
-        return ResponseEntity.ok()
-                .cacheControl(CacheControl.noCache())
-                .lastModified(ZonedDateTime.of(lastModifiedAt, ZoneId.systemDefault()))
-                .body(ApiResponse.success(AllCategoryResponse.fromAllCategories(allCategories)));
+    public ApiResponse<AllCategoryResponse> getAllCategory() {
+        return ApiResponse.success(categoryService.getAllCategory());
     }
 }
--- src/main/java/bokjak/bokjakserver/domain/category/service/CategoryService.java
+++ src/main/java/bokjak/bokjakserver/domain/category/service/CategoryService.java
@@ -2,7 +2,6 @@ package bokjak.bokjakserver.domain.category.service;
 
 import bokjak.bokjakserver.common.dummy.DummyLocationCategory;
 import bokjak.bokjakserver.common.dummy.DummySpotCategory;
-import bokjak.bokjakserver.common.model.BaseEntity;
 import bokjak.bokjakserver.domain.category.dto.CategoryDto.*;
 import bokjak.bokjakserver.domain.category.model.LocationCategory;
 import bokjak.bokjakserver.domain.category.model.SpotCategory;
@@ -12,9 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
 import java.util.List;
-import java.util.NoSuchElementException;
 
 @Service
 @Transactional(readOnly = true)
@@ -23,27 +20,14 @@ public class CategoryService {
     private final LocationCategoryRepository locationCategoryRepository;
     private final SpotCategoryRepository spotCategoryRepository;
 
-    public AllCategories getAllCategory() {
-        List<LocationCategory> locationCategoryList = locationCategoryRepository.findAll();
-        List<SpotCategory> spotCategoryList = spotCategoryRepository.findAll();
-
-        LocalDateTime lastUpdatedTime = resolveLastUpdatedTime(locationCategoryList, spotCategoryList);
-
-        List<LocationCategoryResponse> locationCategoryResponses = locationCategoryList
+    public AllCategoryResponse getAllCategory(){
+        List<LocationCategoryResponse> locationCategoryResponses = locationCategoryRepository.findAll()
                 .stream().map(LocationCategoryResponse::of)
                 .toList();
-        List<SpotCategoryResponse> spotCategoryResponses = spotCategoryList
+        List<SpotCategoryResponse> spotCategoryResponses = spotCategoryRepository.findAll()
                 .stream().map(SpotCategoryResponse::of)
                 .toList();
-        return AllCategories.of(locationCategoryResponses, spotCategoryResponses, lastUpdatedTime);
-    }
-
-    private LocalDateTime resolveLastUpdatedTime(List<LocationCategory> locationCategoryList, List<SpotCategory> spotCategoryList) {
-        List<LocalDateTime> updatedTimeList = new java.util.ArrayList<>();
-        updatedTimeList.addAll(locationCategoryList.stream().map(BaseEntity::getUpdatedAt).toList());
-        updatedTimeList.addAll(spotCategoryList.stream().map(BaseEntity::getUpdatedAt).toList());
-
-        return updatedTimeList.stream().max(LocalDateTime::compareTo).orElseThrow(NoSuchElementException::new);
+        return AllCategoryResponse.of(locationCategoryResponses, spotCategoryResponses);
     }
 
     // Dummy 생성 및 테스트용 메서드
